04a1a87fb76a511882568e6257f460479997801a
James Webber <bunkerprivate@googlemail.com>
2009-03-22 03:07:29 +0000

    Add config file for ffmpegness.

---
+++ build-aux/nerved_config.hpp.cmake
@@ -0,0 +1,14 @@
+/*!
+\file
+\brief Mostly some bet hedging :)
+*/
+#ifndef NERVED_CONFIG_HPP
+#define NERVED_CONFIG_HPP
+
+#cmakedefine HAVE_FFMPEG_LIBAVCODEC_AVCODEC_H
+#cmakedefine HAVE_FFMPEG_AVCODEC_H
+
+#cmakedefine HAVE_FFMPEG_LIBAVFORMAT_AVFORMAT_H
+#cmakedefine HAVE_FFMPEG_AVFORMAT_H
+
+#endif
