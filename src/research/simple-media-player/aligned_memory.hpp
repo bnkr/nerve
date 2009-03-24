a60c5c4e4fae3be6882798852f0fd319b9deaef7
James Webber <bunkerprivate@googlemail.com>
2009-03-24 21:57:27 +0000

    First working version of the file read test.

---
+++ src/research/simple-media-player/aligned_memory.hpp
@@ -0,0 +1,71 @@
+#ifndef ALIGNED_MEMORY_HPP_gidtjkws
+#define ALIGNED_MEMORY_HPP_gidtjkws
+
+#include <cstdlib>
+#include <boost/utility.hpp>
+
+// How can I statically assert that Alignment is a power of two?
+// TODO: unit test this
+// TODO:
+//   It would be nice to have a variable sized buffer here, like bounded_buffer.  In
+//   fact it would be even nicer to write the bounded_* class in terms of this; ie,
+//   optionally give an alligned memory or just a standard memory.  And using an
+//   alloctor pattern wouldn't be bad.
+// TODO:
+//   There's not much point in having this kind of storage unless you can make the
+//   buffer of a variable size; otherwise just use static_aligned_memory.
+template <std::size_t Alignment, class T = uint8_t>
+class aligned_memory : public boost::noncopyable {
+  public:
+    typedef T  value_type;
+    typedef T* pointer_type;
+
+    static const std::size_t alignment = Alignment;
+
+    // \brief Size in num elements.
+    aligned_memory(std::size_t size) { alloc(size); }
+    ~aligned_memory() { std::free(base_); }
+
+    //! \brief Return aligned storage.
+    T *ptr() { return aligned_base_; }
+    const T *ptr() const { return aligned_base_; }
+
+  private:
+    void alloc(std::size_t size) {
+      base_ = (T*) std::malloc(size * sizeof(T) + Alignment);
+      std::size_t remainder = ((std::size_t) base_) % Alignment;
+      aligned_base_ = (T*) (((std::size_t) base_) + remainder);
+      assert(((std::size_t) aligned_base_) % Alignment == 0);
+    }
+
+    T *base_;
+    T *aligned_base_;
+};
+
+//! \brief Size is the number of elements.
+// TODO: unit test this
+template <std::size_t Alignment, std::size_t Size, class T = uint8_t>
+class static_aligned_memory : public boost::noncopyable {
+  public:
+    typedef T  value_type;
+    typedef T* pointer_type;
+
+    static const std::size_t alignment  = Alignment;
+    static const std::size_t size       = Size;
+    static const std::size_t byte_size  = Size * sizeof(T);
+
+    static_aligned_memory() {
+      const std::size_t byte_offset = ((std::size_t) data_) % Alignment;
+      base_ = (T*) (((std::size_t) data_) + byte_offset);
+      assert(((std::size_t) base_) % Alignment == 0);
+    }
+
+    T *ptr() { return base_; }
+    const T *ptr() const { return base_; }
+
+  private:
+    uint8_t data_[Size * sizeof(T) + Alignment];
+    T * const base_;
+};
+
+#endif
--- src/research/simple-media-player/load_file_test.cpp
+++ src/research/simple-media-player/load_file_test.cpp
@@ -11,40 +11,122 @@ void test_loading_log_cb(void *ptr, int level, const char *fmt, va_list args) {
   // last_log.reset();
 }
 
-void load_file_test(const char *file) {
+
+#include <tr1/type_traits> // alignment_of
+#include <cstdlib>
+
+void dump_packet(AVPacket &packet) {
+  std::cout << "Packet data:" << std::endl;
+  std::cout << "  presentation ts:  " << packet.pts << std::endl;
+  std::cout << "  decompression ts: " << packet.dts << std::endl;
+  std::cout << "  data:             " << (void*) packet.data << std::endl;
+  int al;
+  std::size_t input_buf = (std::size_t) packet.data;
+  if (input_buf % 16 == 0) {
+    al = 16;
+  }
+  else if (input_buf % 8 == 0) {
+    al = 8;
+  }
+  else if (input_buf % 4 == 0) {
+    al = 4;
+  }
+  else if (input_buf % 2 == 0) {
+    al = 2;
+  }
+  else if (input_buf % 1 == 0) {
+    al = 1;
+  }
+  else {
+    al = -1;
+  }
+  std::cout << "  data alignment:   " << al << std::endl;
+  std::cout << "  size:             " << packet.size << std::endl;
+  std::cout << "  stream index:     " << packet.stream_index << std::endl;
+  std::cout << "  packet flag:      " << packet.flags << " (" << ((packet.flags == PKT_FLAG_KEY) ? "keyframe" : "non-keyframe") <<  ")" << std::endl;
+  std::cout << "  presentation dur: " << packet.duration << std::endl;
+  std::cout << "  deallocator:      " << (void*) packet.destruct << std::endl;
+  std::cout << "  byte offset:      " << packet.pos << std::endl;
+}
+
+#include "aligned_memory.hpp"
+
+
+// loop through all frames and print out data about them.
+void load_file_test(const char *filename) {
   // http://cekirdek.pardus.org.tr/~ismail/ffmpeg-docs/avformat_8h.html
   // http://cekirdek.pardus.org.tr/~ismail/ffmpeg-docs/avcodec_8h.html
   // http://www.inb.uni-luebeck.de/~boehme/using_libavcodec.html
+  //
+  // http://www.dranger.com/ffmpeg/functions.html - abbreviated function list
+  // http://www.dranger.com/ffmpeg/data.html - abbreviated data type list
 
-  av_log_set_level(AV_LOG_DEBUG);
+  try {
+    ffmpeg::library lib(AV_LOG_DEBUG);
+    ffmpeg::file file(filename);
+    file.dump_format(filename);
 
-  av_register_all();
-  AVFormatContext *format_ctx = NULL;
+    ffmpeg::audio_stream audio(file);
 
-  // I need a get_last_error message function, or at least some kind of errno?.
-  //av_log_set_callback(test_loading_log_cb);
+    // TODO:
+    //   obv. this stuff has to be c++alised.
 
-  // bleh... must be via malloc it seems.
-  if (av_open_input_file(&format_ctx, file, NULL, 0, NULL) != 0) {
-    // TODO: what should the error return be?   I want the error message.
-    std::cerr << "couldnae open file" << std::endl;
-    goto errors;
+    // Output must be 16-byte alligned because SSE needs it.
+    const uint8_t align = 16;
+    // use uint8 so the padding will work.
+    typedef aligned_memory<align, int16_t> aligned_memory_type;
+    const std::size_t buffer_byte_size = AVCODEC_MAX_AUDIO_FRAME_SIZE * 2;
+    assert(buffer_byte_size % sizeof(aligned_memory_type::value_type) == 0);
+    aligned_memory_type mem(buffer_byte_size / sizeof(aligned_memory_type::value_type));
+    int16_t *output_buf = mem.ptr();
+
+    AVPacket packet;
+    while(av_read_frame(&file.format_context(), &packet) >= 0) {
+      dump_packet(packet);
+
+      // Reset this every time because decode writes back to it.
+      int output_buf_size = buffer_byte_size;
+      // It looks like this is always 16 bit aligned, so we don't need to mess
+      // with it.  Docs say it should be at least 4 byte alligned.
+      uint8_t *input_buf = packet.data;
+      int input_buf_size = packet.size;
+
+      std::cout << "Buffer properties:" << std::endl;
+
+      // Again, this does not always appear 16-byte aligned.  Perhaps it's to do with
+      // different file formats?
+      // assert(((std::size_t) input_buf % 16) == 0);
+      // docs say it must be 0 in case of corrupt frames but it isn't.
+      // assert(input_buf[input_buf_size - 1] == 0);
+
+      // TODO: this is necessary:
+      // For the input buffer we over allocate by FF_INPUT_BUFFER_PADDING_SIZE
+      // because optimised readers will read in longer bitlengths.  We never
+      // actually read the full data.
+
+      int size = avcodec_decode_audio2(&audio.codec_context(), output_buf, &output_buf_size, input_buf, input_buf_size);
+
+      std::cout << "Decode data:" << std::endl;
+      std::cout << "  number of bytes used: " << size << std::endl;
+      std::cout << "  output_buf_size:      " << output_buf_size  << " (was given " << buffer_byte_size << ")"<< std::endl;
+
+      if (size < 0) {
+        std::cerr << "noooooo!" << std::endl;
+      }
+      else if (size == 0) {
+        std::cerr << "no frame could be decompressed" << std::endl;
+      }
+
+      // then copy output buf into the audio stream
+      av_free_packet(&packet);
+    }
+
+    std::cout << "Yay!" << std::endl;
+  }
+  catch (ffmpeg::ffmpeg_error &e) {
+    std::cerr << "Phail: " << e.what() << std::endl;
+    exit(EXIT_FAILURE);
   }
 
-  if (av_find_stream_info(format_ctx) != 0) {
-    std::cerr << "av_find_stream_info" << std::endl;
-    goto errors;
-  }
-
-  dump_format(format_ctx, 0, file, 0);
-
-  /// TODO: move through the file but just chuck away the data
-
-  std::cout << "Yay!" << std::endl;
-  return;
-
-errors:
-  std::cerr << "Phail." << std::endl;
-  exit(EXIT_FAILURE);
 }
 
