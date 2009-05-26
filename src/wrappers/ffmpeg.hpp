ba34701eb211b22f6e8efd06a7208c758f8468bc
James Webber <bunkerprivate@googlemail.com>
2009-05-26 14:21:03 +0100

    Move aligned memory to ffmpeg (the only place that uses it.)

--- src/wrappers/ffmpeg.hpp
+++ src/wrappers/ffmpeg.hpp
@@ -1,28 +1,30 @@
 /*!
 \file
-\brief C++ helpers for ffmpeg.
+\ingroup grp_ffmpeg
+\brief C++ interface for ffmpeg audio decoding.
 
 Note that here we take the conventions of ffmpeg where the term `context' means
 informational.
+
+Also, the accessors to the ffmpeg structures are prefixed `av_', for example
+file::av_format_context().  If you start using these structs you're probably
+doing something the library hasn't be been designed for.
 
 Ffmpeg is a pretty complicated library and this wrapper aims to simplify it as
 well as add needed functionality like splitting the stream into packets for sdl
 output.  In order to do this, the classes here wrap the ffmpeg structs and expose
 a subset of their fields.  These are the ones you actually need, rather than
 internal stuff.
+
+This wrapper could hypothetically be expanded to cover video as well as audio,
+but you would need to drop back to ffmpeg again for that.
+
+TODO:
+  Try and merge this with nerve.  It should be entirely possible once we have
+  got rid of the packet state nonsense, and also the hacking gap removal stuff.
 */
 #ifndef FFMPEG_HPP_7awlau1z
 #define FFMPEG_HPP_7awlau1z
-
-#include <iostream>
-#include <cstring>
-#include <cstdlib>
-
-#ifdef __GNUC__
-#define FF_ATTRIBUTE_DEPRECATED __attribute__((deprecated))
-#else
-#define FF_ATTRIBUTE_DEPRECATED
-#endif
 
 // FFmpeg developers hate C++, and not without good reason!  But then I have
 // always been a masochist...
@@ -41,8 +43,13 @@ extern "C" {
 #endif
 }
 
-#include "../include/aligned_memory.hpp"
+#ifdef __GNUC__
+#  define FF_ATTRIBUTE_DEPRECATED __attribute__((deprecated))
+#else
+#  define FF_ATTRIBUTE_DEPRECATED
+#endif
 
+#include "ffmpeg/aligned_memory.hpp"
 #include "ffmpeg/data.hpp"
 #include "ffmpeg/file.hpp"
 #include "ffmpeg/audio_stream.hpp"
@@ -50,11 +57,9 @@ extern "C" {
 #include "ffmpeg/packet_reader.hpp"
 #include "ffmpeg/audio_decoder.hpp"
 
-namespace ffmpeg {
-  typedef ffmpeg::packet frame;
-}
-
-namespace ffmpeg { }
+//! \ingroup grp_ffmpeg
+//! \brief All components of the ffmpeg wrapper.
+namespace ffmpeg {}
 
 #endif
 
