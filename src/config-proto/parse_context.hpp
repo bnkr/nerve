a0eec0402bde1495842b0e241221ae44b6639bbf
James Webber <bunkerprivate@googlemail.com>
2011-02-07 16:40:34 +0000

    Add state managment to the parse_context.

--- src/config-proto/parse_context.hpp
+++ src/config-proto/parse_context.hpp
@@ -2,6 +2,8 @@
 // Distributed under a 3-clause BSD license.  See COPYING.
 
 #include "error_reporter.hpp"
+#include "pipeline_configs.hpp"
+#include "../plugin-proto/asserts.hpp"
 
 #include <boost/utility.hpp>
 
@@ -18,8 +20,37 @@ namespace config {
     error_reporter &reporter() { return reporter_; }
     pipeline_config &output() { return output_; }
 
+    //! \name Parser actions
+    //! These maintain state within a parse.
+    //@{
+
+    void new_job() { current.job = &(output_.new_job()); }
+    void new_section() { current.section = &NERVE_CHECK_PTR(current.job)->new_section(); }
+    void new_stage() { current.stage = &NERVE_CHECK_PTR(current.section)->new_stage(); }
+
+    void end_job() { current.reset(); }
+    void end_section() { current.stage = NULL; current.section = NULL; }
+    void end_stage() { current.stage = NULL; }
+
+    stage_config &this_stage() { return *NERVE_CHECK_PTR(current.stage); }
+    section_config &this_section() { return *NERVE_CHECK_PTR(current.section); }
+    job_config &this_job() { return *NERVE_CHECK_PTR(current.job); }
+
+    //@}
+
     private:
     pipeline_config &output_;
+
+    // Differ depending on where we are in the parse.
+    struct output_pointers {
+      job_config *job;
+      section_config *section;
+      stage_config *stage;
+
+      output_pointers() { reset(); }
+      void reset() { job = NULL; section = NULL; stage = NULL; }
+    } current;
+
     error_reporter reporter_;
   };
 }
