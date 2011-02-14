4e32e4e61c08ad044510949650e50d09534161e6
James Webber <bunkerprivate@googlemail.com>
2011-02-14 14:07:49 +0000

    Initial work on logging framework.

--- src/nerved/output/ideas.cpp
+++
@@ -1,8 +0,0 @@
-  enum types {
-    //! Error with configuration.  This gets put on stderr.
-    err_configure
-  };
-
-  // meh.. don't like this too much -- would prefer a class which isn't global
-  void log(enum types, const char *format, ...);
-
